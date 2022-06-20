dc86e70acc00b323389ec5e22171ceb4fabe17ce
Данияр Куанышбаев <danik.kuanyshbayev@gmail.com>
2022-06-20 19:32:09 +0600

    hw

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
+import React  from 'react';
 import './App.css';
+import logo from "./img/logo.jpeg";
+// import {useState} from 'react';
+import { format } from 'date-fns';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <div>
-        hello world
+  const currentDate=new Date();
+   const dadate=format(currentDate, 'MM.dd.yyyy hh:mm')
+  
+  return(
+    <> <header > <img src={logo} /> 
+       { dadate }
+       {console.log('qweqwe')}
+      </header>
+    <div className="App"> 
+        
+        <div className="HEY qq"><p>HEY</p> </div>
+
+
+        <div className="LETS qq"><p>LET'S </p></div>
+
+
+        <div className="GIVE qq"><p>GIVE</p> </div>
+
+
+        <div className="ALL qq"><p>ALL</p> </div>
+
+
+        <div className="YOUCAN qq"><p>YOU CAN</p></div>
+
       </div>
-      <div>
-        hello world 2
-      </div>
-    </div>
+    </>
+    
   );
 }
 
